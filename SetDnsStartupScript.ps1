97de4f9542b00fc54e5cd213a60af40d55026c60
Rogan Helm <roganhelm@proton.me>
2024-05-14 23:00:39 -0500

    Add files via upload

---
+++ SetDnsStartupScript.ps1
@@ -0,0 +1,10 @@
+﻿$primaryDNS = "192.168.50.4"
+$secondaryDNS = "192.168.50.5"
+$tertiaryDNS = "208.67.220.220"
+
+$adapters = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
+
+foreach ($adapter in $adapters) {
+  Set-DnsClientServerAddress -InterfaceIndex $adapter.ifIndex -ServerAddresses ($primaryDNS, $secondaryDNS, $tertiaryDNS)
+  Write-Host "Successfully set DNS servers for adapter: $($adapter.Name)"
+}
