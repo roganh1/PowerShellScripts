6dea5b81a44b013557653140945e84bbcd07cb05
Rogan Helm <roganhelm@proton.me>
2024-05-24 20:46:39 -0500

    Add files via upload

---
+++ DisableLocalUsers.ps1
@@ -0,0 +1,12 @@
+﻿$DomainComputers = Get-ADGroupMember -Identity "Domain Computers" | Select-Object -ExpandProperty Name
+$Session = New-PSSession -ComputerName $DomainComputers -Credential AD\radmin
+$Exceptions = @("Administrator", "WDAGUtilityAccount")
+
+Invoke-Command -Session $Session {
+    Get-LocalUser | Where-Object {
+        $_.Enabled -eq $true -and $_.Name -notin $Exceptions
+        } | ForEach-Object {
+            Disable-LocalUser $_
+            }
+}
+Get-PSSession | Remove-PSSession
